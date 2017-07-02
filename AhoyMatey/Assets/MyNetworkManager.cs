9ba12fec315cd918099349405034c470100895d0
Tobias Trabant <tobias.trabant@gmx.de>
2017-07-02 17:36:56 +0200

    commit1

---
+++ AhoyMatey/Assets/MyNetworkManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class MyNetworkManager : NetworkManager {
+
+
+
+    public void MyStartHost()
+    {
+        StartHost();
+        Debug.Log(Time.timeSinceLevelLoad + " MyStartHost");
+    }
+    
+
+    public override void OnStartHost()
+    {
+        Debug.Log(Time.timeSinceLevelLoad + " OnStartHost");
+    }
+
+    public override void OnStartClient(NetworkClient myClient)
+    {
+        Debug.Log(Time.timeSinceLevelLoad + " Client start requested");
+        base.OnStartClient(myClient);
+    }
+
+    public override void OnClientConnect(NetworkConnection conn)
+    {
+        Debug.Log(Time.timeSinceLevelLoad + " Client is connected to ip: " + conn.address);
+        base.OnClientConnect(conn);
+    }
+}
